76c83c588d80b71580b6fccbc61844f7a4e94f08
Kevin Riggs <riggskevinp@gmail.com>
2022-05-07 18:32:08 -0500

    Render detailed image

--- main.cpp
+++ main.cpp
@@ -29,18 +29,64 @@ color ray_color(const ray& r, const hittable& world, int depth){
 }
 
 struct Image {
-    static constexpr auto const aspect_ratio = 16.0 / 9.0;
-    static constexpr auto const width = 400;
+    static constexpr auto const aspect_ratio = 3.0 / 2.0;
+    static constexpr auto const width = 1200;
     static constexpr auto const height = static_cast<int>(width / aspect_ratio);
-    static constexpr auto const samples_per_pixel = 100;
+    static constexpr auto const samples_per_pixel = 500;
     static constexpr auto const max_depth = 50;
-    static constexpr auto const look_from = point3{3,3,2};
-    static constexpr auto const look_at = point3{0,0,-1};
+    static constexpr auto const look_from = point3{13,2,3};
+    static constexpr auto const look_at = point3{0,0,0};
     static constexpr auto const vup = vector3{0,1,0};
-    static constexpr auto const dist_to_focus = (look_from - look_at).length();
-    static constexpr auto const aperture = 2.0;
+    static constexpr auto const dist_to_focus = 10.0;
+    static constexpr auto const aperture = 0.1;
     static constexpr auto const vertical_fov = 20.0;
 };
+
+hittable_list random_scene(){
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5,0.5,0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    for(int a = -11; a < 11; a++){
+        for(int b = -11; b < 11; b++){
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if((center - point3(4, 0.2, 0)).length() > 0.9){
+                shared_ptr<material> sphere_material;
+
+                if(choose_mat < 0.8){
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if(choose_mat < 0.95){
+                    // metal
+                    auto albedo = color::random(0.5,1);
+                    auto fuzz = random_double(0,0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else{
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0,1,0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4,0.2,0.1));
+    world.add(make_shared<sphere>(point3(-4,1,0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7,0.6,0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4,1,0), 1.0, material3));
+
+    return world;
+}
 
 void render_helloworld(){
     /*
@@ -51,23 +97,9 @@ void render_helloworld(){
     const Image img;
 
     // World
-    hittable_list world;
-
-    auto material_ground = make_shared<lambertian>(color(0.8,0.8,0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<metal>(color(0.8,0.6,0.2), 0.0);
-
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    // trick for hollow spheres: negative radius doesn't affect geometry but the surface normal points outward
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
-    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
-
+    hittable_list world = random_scene();
 
     // Camera
-
     Camera cam(img.look_from, img.look_at, img.vup, img.vertical_fov, img.aspect_ratio, img.aperture, img.dist_to_focus);
 
     // Render
